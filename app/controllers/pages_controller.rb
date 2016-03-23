1d3ea22468a81bc1abe983a5e7afb83512e57a2e
Alison Ostendorf <aostendorf@gmail.com>
2016-03-22 22:05:34 -0700

    added route for receipt_path and scaffold of page info

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,7 @@ class PagesController < ApplicationController
   def order_page
   end
 
+  def confirmation
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root "pages#home"
   get "order_page" => "pages#order_page"
+  get "confirmation" => "pages#confirmation"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
