04b2b521600943f617f3614c6777ff96245fc126
Alison Ostendorf <aostendorf@gmail.com>
2016-03-22 22:14:27 -0700

    add FAQ paths and view, and remove faq text from order page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,4 +8,7 @@ class PagesController < ApplicationController
   def confirmation
   end
 
+  def faq
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root "pages#home"
   get "order_page" => "pages#order_page"
   get "confirmation" => "pages#confirmation"
+  get "faq" => "pages#faq"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
