b8f7385c47edebb3629f8d01ca87b9cc5cab0fff
Alison Ostendorf <aostendorf@gmail.com>
2016-09-12 18:14:46 -0400

    work on drop down of kit contents on small screens, implement toggle with down and up arrows

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
 	$("#click-this").click(function() {
 		$("#drop-this").toggle("fast");
 	});
+
+	$(".category_name").click(function() {
+		$(this).find(".down-arrow").toggle();
+		$(this).find(".up-arrow").toggle();
+		$(this).next("ul").slideToggle();
+	});
 });
